5c6fd2f6d4749a3780a12e43a937a157ae0fb8a8
Volkan <volkan@example.com>
2026-04-08 03:46:36 +0300

    Gereksiz dosyalar kaldirildi

--- test.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const code = fs.readFileSync('index.html', 'utf8');
-const str = code.substring(code.indexOf('const gonderilecekVarMi'), code.indexOf('<div class="butonlar">') + 20);
-fs.writeFileSync('temp.txt', str);
